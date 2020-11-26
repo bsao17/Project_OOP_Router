7bb52c86af2f18ba7819dd726db47b6b194007cb
bsao17 <greta.b.meddhed>
2020-11-26 15:49:22 +0100

    Router class implementation

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
 <?php
+
+require "vendor/autoload.php";
+
+use controllers\Router;
+
+if(isset($_GET['action'])){
+    $action = $_GET['action'];
+}else{
+    $action = null;
+}
+
+if($action != null){
+    switch($action){
+        case "home":
+            $route = new Router();
+            require Router::LINK_HOME;
+            break;
+
+        case "signin":
+            require_once Router::LINK_SIGNIN;
+            Dump($_POST);
+            break;
+
+        default:
+            echo('404 not found');
+    }
+
+}
---
+++ src/controllers/Router.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace controllers;
+
+class Router{
+    /**
+     *constant link
+     */
+    const LINK_HOME = "src/views/home.html";
+    const LINK_SIGNIN = "src/views/signin.html";
+}
