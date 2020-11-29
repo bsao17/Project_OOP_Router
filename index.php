73ae57dd1e94b648c585e7cb3374cdd3c5eeeb76
bsao17 <declic62@gmail.com>
2020-11-29 08:56:07 +0100

    HTTPFoundation request action

--- index.php
+++ index.php
@@ -1,29 +1,54 @@
 <?php
 
 require "vendor/autoload.php";
-$loader = new \Twig\Loader\FilesystemLoader('src/views/templates');
-$twig = new \Twig\Environment($loader, [
-    'cache' => false,
-]);
+use Symfony\Component\HttpFoundation\Request;
 
-use controllers\Router;
+$request = Request::createFromGlobals();
 
-$action = isset($_GET['action']) ? $_GET['action'] : null;
+$request = new Request(
+    $_GET,
+    $_POST,
+    [],
+    $_COOKIE,
+    $_FILES,
+    $_SERVER
+);
 
-if($action != null){
-    switch($action){
-        case "home":
-            $route = new Router();
-            echo $twig->render('home.twig');
-            break;
+$links = ["home", "contact", "billets"];
+$action = $request->query->get('action');
 
-        case "signin":
-            require_once Router::LINK_SIGNIN;
-            Dump($_POST);
-            break;
-
-        default:
-            echo('404 not found');
-    }
-
+if( in_array( $action, $links)){
+    echo sprintf("c'est bien %s ", $action);
+}else{
+    echo "404 not found !!!";
 }
+
+
+
+// $loader = new \Twig\Loader\FilesystemLoader('src/views/templates');
+// $twig = new \Twig\Environment($loader, [
+//     'cache' => false,    
+// ]);
+
+// use controllers\Router;
+
+// $action = isset($_GET['action']) ? $_GET['action'] : null;
+
+// if($action != null){
+//     switch($action){
+//         case "home":
+//             $route = new Router();    
+//             echo $twig->render('home.twig');
+//             break;
+
+//         case "signin":
+//             require_once Router::LINK_SIGNIN;    
+//             Dump($_POST);
+//             break;
+
+//         default:
+//             echo('404 not found');
+//     }
+
+// }
+ 
